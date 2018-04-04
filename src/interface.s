94e7769014a1b33dcfe275d5aa2afa9fe5013504
Bazyli Cyran <bazyli.cyran@gmail.com>
2018-04-05 00:17:50 +0200

    User interface related functions in interface.s file

---
+++ src/interface.s
@@ -0,0 +1,50 @@
+#
+# Functions related to user interface
+# 
+
+#
+# Constants
+#
+SYS_READ = 0
+SYS_WRITE = 1
+SYS_EXIT = 60
+STD_IN = 0
+STD_OUT = 1
+EXIT_SUCCESS = 0
+INPUT_LEN = 1024
+
+#
+# Global functions decalrations
+#
+.global show_greeting
+.global show_prompt
+.global exit
+
+#
+# Show greeting text
+#
+show_greeting:
+	movq	$SYS_WRITE, %rax
+	movq	$STD_OUT, %rdi
+	movq	$greeting, %rsi
+	movq	$greeting_len, %rdx
+	syscall
+
+#
+# Show prompt
+#
+show_prompt:
+	movq	$SYS_WRITE, %rax
+	movq	$STD_OUT, %rdi
+	movq	$prompt, %rsi
+	movq	$prompt_len, %rdx
+	syscall
+
+#
+# Exit from program with success code
+#
+exit:
+	movq	$SYS_EXIT, %rax
+	movq	$EXIT_SUCCESS, %rdi
+	syscall
+
--- src/main.s
+++ src/main.s
@@ -5,19 +5,6 @@
 # Initialized data
 #
 .data
-	# System calls
-	SYS_READ = 0
-	SYS_WRITE = 1
-	SYS_EXIT = 60
-
-	# I/O
-	STD_IN = 0
-	STD_OUT = 1
-
-	# Others
-	EXIT_SUCCESS = 0
-	INPUT_LEN = 1024
-	
 	# Greeting message shown on program start
 	greeting: .ascii "Kalkulator wyrażeń w odwrotnej notacji polskiej, podaj wyrażenie po znaku '>'.\n"
 	greeting_len = . - greeting
@@ -26,7 +13,6 @@
 	prompt: .ascii "> "
 	prompt_len = . - prompt
 
-
 #
 # Uninitialized data
 #
@@ -34,38 +20,24 @@
 	# User input buffer
 	.comm input, 1024
 
-
 #
 # Program
 #
 .text
 	.global main
-
+	.global	greeting
+	.global greeting_len
+	.global prompt
+	.global	prompt_len
 
 #
 # Entry point
 #
 main:
 
+	# Show greeting
+	call	show_greeting
 
-#
-# Show greeting text
-#
-greet:
-	movq	$SYS_WRITE, %rax
-	movq	$STD_OUT, %rdi
-	movq	$greeting, %rsi
-	movq	$greeting_len, %rdx
-	syscall
-
-
-// TODO Well, the entire program
-
-#
-# Exit from program with success code
-#
-exit:
-	movq	$SYS_EXIT, %rax
-	movq	$EXIT_SUCCESS, %rdi
-	syscall
-
+	# Exit from program
+	call	exit
+	
