a98df487de46fc9570881514bc8cf9dcf447c9dc
Bazyli Cyran <bazyli.cyran@gmail.com>
2018-04-04 21:38:22 +0200

    Reformat to more readable form

--- main.s
+++ main.s
@@ -1,40 +1,56 @@
-# SYSTEM CALLS
-SYS_READ = 0
-SYS_WRITE = 1
-SYS_EXIT = 60
+# asm-rpm-calc: simple reverse polish notation calculator
+# Authors: Bazyli Cyran, Paweł Komorowski
 
-# I/O
-STD_IN = 0
-STD_OUT = 1
-
-# OTHERS
-EXIT_SUCCESS = 0
-INPUT_LEN = 1024
-
-
-# INITIALIZED BUFFERS
+#
+# Initialized data
+#
 .data
-# Greeting message shown on program start
-greeting: .ascii "Kalkulator wyrażeń w odwrotnej notacji polskiej, podaj wyrażenie po znaku '>'.\n"
-greeting_len = . - greeting
-# Prompt for next calculation
-prompt: .ascii "> "
-prompt_len = . - prompt
+	# System calls
+	SYS_READ = 0
+	SYS_WRITE = 1
+	SYS_EXIT = 60
+
+	# I/O
+	STD_IN = 0
+	STD_OUT = 1
+
+	# Others
+	EXIT_SUCCESS = 0
+	INPUT_LEN = 1024
+	
+	# Greeting message shown on program start
+	greeting: .ascii "Kalkulator wyrażeń w odwrotnej notacji polskiej, podaj wyrażenie po znaku '>'.\n"
+	greeting_len = . - greeting
+
+	# Prompt for next calculation
+	prompt: .ascii "> "
+	prompt_len = . - prompt
 
 
-# UNINITIALIZED BUFFERS
+#
+# Uninitialized data
+#
 .bss
-# User input buffer
-.comm input, 1024
+	# User input buffer
+	.comm input, 1024
 
 
-# PROGRAM
+#
+# Program
+#
 .text
-.global main
+	.global main
+
+
+#
+# Entry point
+#
 main:
 
 
+#
 # Show greeting text
+#
 greet:
 	movq	$SYS_WRITE, %rax
 	movq	$STD_OUT, %rdi
@@ -45,8 +61,9 @@ greet:
 
 // TODO Well, the entire program
 
-
+#
 # Exit from program with success code
+#
 exit:
 	movq	$SYS_EXIT, %rax
 	movq	$EXIT_SUCCESS, %rdi
