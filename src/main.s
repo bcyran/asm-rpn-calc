c10581d42804fecb75e6b42797f44ae859211e40
Bazyli Cyran <bazyli.cyran@gmail.com>
2018-04-08 17:52:53 +0200

    Display result after entering expression

--- src/main.s
+++ src/main.s
@@ -21,6 +21,7 @@
 .bss
 	.comm input, 1024			# User input buffer
 	.comm cur_token, 1024			# Token storage for parser
+	.comm output, 1024			# Output buffer
 	.comm tmp, 1024				# Temporary storage space
 
 
@@ -49,14 +50,26 @@ main:
 #
 main_loop:
 	call	show_prompt			# Show prompt
-	call	read_input			# Read user input
 
-	movq	$input, %rdi			# Parameter for calculate function (input buffer)
-	call	calculate			# Calculate value of the expression
+	movq	$input, %rdi			# Parameter for clean_buffer (buffer to clean)
+	movq	$INPUT_LEN, %rsi		# Parameter for clean_buffer (length of the buffer)
+	call	clear_buffer			# Clear input buffer
+
+	call	read_input			# Read user input
 	
-	test:
+	cmpb	$'q', input			# If entered char is 'q'
+	je	exit				# End program
+
+	movq	$input, %rdi			# Parameter for calculate (input buffer)
+	call	calculate			# Calculate value of the expression
+
+	movq	$output, %rdi			# Parameter for ftoa (output buffer)
+	movq	$2, %rsi			# Parameter for ftoa (precision)
+	call	ftoa				# Convert result to ASCII string
+
+	movq	$output, %rdi			# Parameter for print_string (buffer to print)
+	movq	%rax, %rsi			# Parameter fro print_string (number of char to print)
+	call	print_string			# Print result
 
 	jmp	main_loop			# Jump to the start of the loop
 
-	call	exit				# Exit from program
-	
